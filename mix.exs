5b5641275296d22ef35e81fdf69c232a86a55fbb
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-19 22:12:51 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceKarma.Mixfile do
 
   def project do
     [app: :alice_karma,
-     version: "0.3.2",
+     version: "0.3.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
